561e182814394fdccd2e1fce3e270bbcee838550
unknown <ivanchinocastro773@gmail.com>
2021-11-05 13:43:25 -0300

    arregle ejer10

--- clase10/ejer10.js
+++ clase10/ejer10.js
@@ -4,8 +4,8 @@ let cad2 = "ivan castro";
 function concatTwoString(str1,str2) {
     let cadena1 = str1.split('');
     let cadena2 = str2.split(''); //convierto los dos string en array
-    let removed1 = cadena1.shift(); //elimino los dos primeros elementos(letra).
-    let removed2 = cadena2.shift();
+    let removed1 = cadena1.splice(0,2);//elimino los dos primeros elementos(letra).
+    let removed2 = cadena2.splice(0,2);
     cadena1 = cadena1.join(""); //lo paso nuevamente a string
     cadena2 = cadena2.join("");
     
