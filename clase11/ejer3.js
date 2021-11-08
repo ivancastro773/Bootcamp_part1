dfb4d973a203861bf42a513c6a76df7c08c82803
unknown <ivanchinocastro773@gmail.com>
2021-11-08 15:21:12 -0300

    hice aclaraciones

--- clase11/ejer3.js
+++ clase11/ejer3.js
@@ -1,11 +1,11 @@
 let str = "index.html";
 let srt2 = "hola ivan.js";
 function extension(cadena) {
-    if (cadena.indexOf(".")) {
+    if (cadena.indexOf(".")) { //pregunto si la cadena tiene extension
         let ext = cadena.split(".");
         return ext[1];
     }else{
-        return "La cadena no posee un . (punto)";
+        return "La cadena no posee una extension";
     }
 }
 console.log(extension(str));
--- clase11/ejer5.js
+++ clase11/ejer5.js
@@ -1,7 +1,7 @@
 
 function dataDay() {
     let date = new Date();
-    let hour = date.getHours()+":"+date.getMinutes();
+    let hour = date.getHours()+":"+date.getMinutes(); 
     let days = ["Domingo","Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
     let day = days[date.getDay()];
     return "Hoy es "+day+" y la hora es "+hour;
