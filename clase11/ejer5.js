70c1a966c530f0942e6aae5a9934c42398041141
unknown <ivanchinocastro773@gmail.com>
2021-11-08 13:14:34 -0300

    arregle el ejer5.js

--- clase11/ejer5.js
+++ clase11/ejer5.js
@@ -1,16 +1,9 @@
 
 function dataDay() {
-    let days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
-    let i = 0;
-    let j=days.length;
     let date = new Date();
-    let day;
     let hour = date.getHours()+":"+date.getMinutes();
-    for (i; i < j; i++) {
-        if (date.getDay()-1 == i) {
-            day = days[i];
-        }
-    }
+    let days = ["Domingo","Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    let day = days[date.getDay()];
     return "Hoy es "+day+" y la hora es "+hour;
 }
 console.log(dataDay());
