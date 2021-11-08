aea5de5a7cf5d02f3cc77e52bcd0e6d8bb70c527
unknown <ivanchinocastro773@gmail.com>
2021-11-08 15:15:53 -0300

    arregle ejer3.js

--- clase11/ejer3.js
+++ clase11/ejer3.js
@@ -1,7 +1,12 @@
 let str = "index.html";
-
+let srt2 = "hola ivan.js";
 function extension(cadena) {
-    let ext = cadena.split(".");
-    return ext[1];
+    if (cadena.indexOf(".")) {
+        let ext = cadena.split(".");
+        return ext[1];
+    }else{
+        return "La cadena no posee un . (punto)";
+    }
 }
 console.log(extension(str));
+console.log(extension(srt2));
